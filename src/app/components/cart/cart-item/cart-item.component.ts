138b8d34264308d0812a6e0a23961944654809bf
PGaillot <pr.gaillot@gmail.com>
2024-06-28 12:14:24 +0200

    add item cart behavior.

--- src/app/components/cart/cart-item/cart-item.component.ts
+++ src/app/components/cart/cart-item/cart-item.component.ts
@@ -1,5 +1,13 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { Item } from '../../../models/hideout-item.model';
+import { HideoutDetailService } from '../../../services/hideout-detail.service';
 
 @Component({
   selector: 'app-cart-item',
@@ -7,20 +15,59 @@ import { Item } from '../../../models/hideout-item.model';
   imports: [],
   styleUrl: './cart-item.component.scss',
   template: `
-      <div id="cart-item">
-        <p>x{{item.quantity}}</p>
-        <img [src]="item.iconLink" [alt]="item.name+'\'icon.'" />
+    <div id="cart-item">
+      <div #cartWrapper id="cart-wrapper">
+        <p>x{{ item.quantity }}</p>
+        <img [src]="item.iconLink" [alt]="item.name + ' icon.'" />
         <div>
-          <div id="name">{{ item.name }}</div>
+          <div #itemName id="name">
+            <span #crossOut></span>
+            {{ item.name }}
+          </div>
           <div id="goal">Vent lvl 2</div>
         </div>
-        <button id="found-btn" (click)="foundClick.emit($event)">found!</button>
-        <button class="stealth" (click)="deleteClick.emit($event)">delete</button>
+        <button id="found-btn" (click)="foundClick()">found!</button>
+        <button class="stealth" (click)="deleteClick()">delete</button>
       </div>
+    </div>
   `,
 })
 export class CartItemComponent {
   @Input() item!: Item;
-  @Output() deleteClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
-  @Output() foundClick: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+  @ViewChild('cartWrapper', { static: true }) cartItemRef!: ElementRef;
+  @ViewChild('crossOut', { static: true }) crossOutRef!: ElementRef;
+  @ViewChild('itemName', { static: true }) itemNameRef!: ElementRef;
+
+  constructor(private hideoutDetailService: HideoutDetailService) {}
+
+  deleteClick() {
+    // 1- Cible les éléments à animer
+    const cartElement: HTMLElement = this.cartItemRef.nativeElement;
+
+    this.hideoutDetailService.deleteCartItem(this.item.id).subscribe({
+      next: (value) => {
+        // 2 - ajoute là class 'animation sur l'element ciblé à animer
+        cartElement.classList.add('deleted');
+      },
+      error: (err) => {
+        // 2 - ajoute là class 'animation sur l'element ciblé à animer
+        cartElement.classList.add('delete-error');
+        console.error(err)
+      },
+    });
+  }
+
+  foundClick() {
+    // 1 - Cible les éléments à animer
+    const crossOutElement: HTMLElement = this.crossOutRef.nativeElement;
+    const itemNameElement: HTMLElement = this.itemNameRef.nativeElement;
+    const cartElement: HTMLElement = this.cartItemRef.nativeElement;
+
+    //TODO: Ajouter methode pour confirmer comme trouvé
+
+    // 2 - ajoute là class 'animation sur l'element ciblé à animer
+    crossOutElement.classList.add('cross-out');
+    itemNameElement.classList.add('item-name-found');
+    cartElement.classList.add('deleted-delay')
+  }
 }
